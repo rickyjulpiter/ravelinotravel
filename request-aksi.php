164b89ea1ea32255924f3e5cbfaf54968215d08b
rickyjulpiter <rickyjulpiter14@gmail.com>
2019-10-22 01:45:44 +0700

    change request and message to PDO

--- request-aksi.php
+++ request-aksi.php
@@ -1,18 +1,30 @@
 <?php
     include 'koneksi.php';
+    include 'admin/login/config.php';
 
     $nama = $_POST['nama'];
     $email = $_POST['email'];
     $telepon = $_POST['telepon'];
     $pesan = $_POST['pesan'];
 
-    $queryInsert = "INSERT INTO request (nama,email,telepon,pesan) VALUES ('$nama','$email','$telepon','$pesan')";
-    mysqli_query($koneksi,$queryInsert);
-    //echo($queryInsert);
-    echo "<script>
-	alert('Your request has been sent, thank you!');
-	window.location.href='request';
-	</script>";
-    //header("location:contactus");
+    $sql = "INSERT INTO request (nama,email,telepon,pesan) VALUES (:nama,:email,:telepon,:pesan)";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':nama',$nama,PDO::PARAM_STR);
+    $query->bindParam(':email',$email,PDO::PARAM_STR);
+    $query->bindParam(':telepon',$telepon,PDO::PARAM_STR);
+    $query->bindParam(':pesan',$pesan,PDO::PARAM_STR);
+    $query->execute();
+    $lastInsertId = $dbh->lastInsertId();
+    if($lastInsertId)
+    {
+        echo "<script>
+        alert('Your request has been sent, thank you!');
+        window.location.href='request';
+        </script>";
+    }
+    else 
+    {
+        echo "<script>alert('Something went wrong. Please try again');</script>";
+    }
     
 ?>
