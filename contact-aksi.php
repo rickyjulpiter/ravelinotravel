aa1213b91f79d7cfe16160235553f337d85bb849
rickyjulpiter <rickyjulpiter14@gmail.com>
2019-11-26 12:58:51 +0700

    email updated

--- contact-aksi.php
+++ contact-aksi.php
@@ -1,11 +1,6 @@
 <?php
     include 'config.php';
 
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $telepon = $_POST['telepon'];
-    $pesan = $_POST['pesan'];
-
     $captcha = $_POST['captcha'];
     $verif = $_POST['verif'];
 
@@ -16,24 +11,31 @@
             </script>";
     }
     else if ($captcha == $verif) {
-        $sql = "INSERT INTO pesan (nama,email,telepon,pesan) VALUES (:nama,:email,:telepon,:pesan)";
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':nama',$nama,PDO::PARAM_STR);
-        $query->bindParam(':email',$email,PDO::PARAM_STR);
-        $query->bindParam(':telepon',$telepon,PDO::PARAM_STR);
-        $query->bindParam(':pesan',$pesan,PDO::PARAM_STR);
-        $query->execute();
-        $lastInsertId = $dbh->lastInsertId();
-        if($lastInsertId)
-        {
-            echo "<script>
-            alert('Your message has been sent, thank you!');
-            window.location.href='contactus';
-            </script>";
-        }
-        else 
-        {
-            echo "<script>alert('Something went wrong. Please try again');</script>";
-        }
+        // $sql = "INSERT INTO pesan (nama,email,telepon,pesan) VALUES (:nama,:email,:telepon,:pesan)";
+        // $query = $dbh->prepare($sql);
+        // $query->bindParam(':nama',$nama,PDO::PARAM_STR);
+        // $query->bindParam(':email',$email,PDO::PARAM_STR);
+        // $query->bindParam(':telepon',$telepon,PDO::PARAM_STR);
+        // $query->bindParam(':pesan',$pesan,PDO::PARAM_STR);
+        // $query->execute();
+        // $lastInsertId = $dbh->lastInsertId();
+        // if($lastInsertId)
+        // {
+        //     echo "<script>
+        //     alert('Your message has been sent, thank you!');
+        //     window.location.href='contactus';
+        //     </script>";
+        // }
+        $to = "master@sistempintar.com";
+        $nama = $_POST['nama'];
+        $email = $_POST['email'];
+        $telepon = $_POST['telepon'];
+        $pesan = $_POST['pesan'];
+
+        mail($to, "Contact Us Page", $pesan." Telepon : ".$telepon, "FROM : ".$email);
+    } 
+    else {
+    echo "<script>alert('Something went wrong. Please try again');</script>";
+}
     }   
 ?>
